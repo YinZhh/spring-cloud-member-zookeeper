1682793b99ff2bba490968dc39b9ec49fa0cb6d7
YinZhh <1335263679@qq.com>
2018-12-29 17:52:45 +0800

    DiscoveryClient

--- src/main/java/com/zhh/api/constroller/ZkMemberApiController.java
+++ src/main/java/com/zhh/api/constroller/ZkMemberApiController.java
@@ -1,8 +1,16 @@
 package com.zhh.api.constroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName: 会员服务控制层
@@ -16,9 +24,29 @@ public class ZkMemberApiController {
 
     @Value("${server.port}")
     private String serverPort;
+    @Value("${spring.application.name}")
+    private String instanceName;
+
+    private final DiscoveryClient discoveryClient;
+
+    @Autowired
+    public ZkMemberApiController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
 
     @RequestMapping("/getMember")
     public String getmember() {
-        return "this is member，我是会员zookeeper服务  端口号： " + serverPort;
+        return "this is member，我是会员服务:" + instanceName + "  端口号： " + serverPort;
+    }
+
+    @GetMapping("/services")
+    public List<String> serviceUrl() {
+        List<String> clientServices = discoveryClient.getServices();
+        List<ServiceInstance> list = discoveryClient.getInstances(instanceName);
+        List<String> services = new ArrayList<>();
+        if (list != null && list.size() > 0) {
+            list.forEach(serviceInstance -> services.add(serviceInstance.getUri().toString()));
+        }
+        return services;
     }
 }
