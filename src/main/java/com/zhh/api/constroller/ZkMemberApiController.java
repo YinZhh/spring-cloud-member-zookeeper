b865a492c5e2acdc94d4b04a91f1827e58c8020d
yin.zhh <1335263679@qq.com>
2019-01-01 19:11:53 +0800

    spring cloud zookeeper 做注册中心

--- src/main/java/com/zhh/api/constroller/ZkMemberApiController.java
+++ src/main/java/com/zhh/api/constroller/ZkMemberApiController.java
@@ -1,8 +1,12 @@
 package com.zhh.api.constroller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @ClassName: 会员服务控制层
@@ -17,8 +21,23 @@ public class ZkMemberApiController {
     @Value("${server.port}")
     private String serverPort;
 
+    private final DiscoveryClient discoveryClient;
+
+    public ZkMemberApiController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
     @RequestMapping("/getMember")
     public String getmember() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("yinzhh-member-zk");
+        List<String> services = discoveryClient.getServices();
+        System.out.println(services);
+
+        for (ServiceInstance instance : instances) {
+            String scheme = instance.getScheme();
+            System.out.println(scheme);
+        }
+
         return "this is member，我是会员zookeeper服务  端口号： " + serverPort;
     }
 }
